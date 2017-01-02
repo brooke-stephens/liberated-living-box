957133d7d3888d706a0d6f7cd62df1879b93c417
orbithub <brooke.stephens@gmail.com>
2017-01-02 11:56:06 -0500

    add local configs

---
+++ conf/grants.sql
@@ -0,0 +1,5 @@
+# grants.sql
+
+grant all privileges on liberatedliving.* to 'liberatedliving'@'%' identified by 'o123';
+
+flush privileges;
--- local-script.sh
+++ local-script.sh
@@ -24,58 +24,61 @@ then
 fi
 sudo ln -s /vagrant/conf/liberatedliving-apache2.conf $APACHE_CONF_FILE
 
-echo "######## SETTING UP LIBERATEDLIVING DB ##############"
+# echo "######## SETTING UP LIBERATEDLIVING DB ##############"
 
-# DB_IMPORT_URL=https://dl.dropboxusercontent.com/...
-#echo "Downloading database snapshot from $DB_IMPORT_URL"
-#curl -o $DB_IMPORT_FILE=$DB_IMPORT_FILE
+# # DB_IMPORT_URL=https://dl.dropboxusercontent.com/...
+# #echo "Downloading database snapshot from $DB_IMPORT_URL"
+# #curl -o $DB_IMPORT_FILE=$DB_IMPORT_FILE
 
 
-# import the database
-# mysqldump -h orbit-development.cb0nvxtqnr3s.us-east-1.rds.amazonaws.com --ssl-ca=/vagrant/conf/rds-combined-ca-bundle.pem --ssl-verify-server-cert -u root --password=5c50af7a5970f88e3628432aa7116596 -e liberatedliving | gzip > /vagrant/conf/liberatedliving.sql.gz
-DB_IMPORT_FILE=/vagrant/conf/liberatedliving.sql.gz
+# # import the database
+# # mysqldump -h orbit-development.cb0nvxtqnr3s.us-east-1.rds.amazonaws.com --ssl-ca=/vagrant/conf/rds-combined-ca-bundle.pem --ssl-verify-server-cert -u root --password=5c50af7a5970f88e3628432aa7116596 -e liberatedliving | gzip > /vagrant/conf/liberatedliving.sql.gz
+# DB_IMPORT_FILE=/vagrant/conf/liberatedliving.sql.gz
 
-if [ -f $DB_IMPORT_FILE ]; then
-	TMP_FILE=/tmp/liberatedliving.sql
-	TMP_FILE_GZ=$TMP_FILE.gz
+# if [ -f $DB_IMPORT_FILE ]; then
+# 	TMP_FILE=/tmp/liberatedliving.sql
+# 	TMP_FILE_GZ=$TMP_FILE.gz
 
-	# sudo rm $TMP_FILE
-	cp -f $DB_IMPORT_FILE $TMP_FILE_GZ
-	gunzip -f $TMP_FILE_GZ
+# 	# sudo rm $TMP_FILE
+# 	cp -f $DB_IMPORT_FILE $TMP_FILE_GZ
+# 	gunzip -f $TMP_FILE_GZ
 
-	echo "Importing/overriding liberatedliving DB from $DB_IMPORT_FILE"
-	mysql -h 127.0.0.1 -u root --password=root -e "CREATE DATABASE liberatedliving CHARACTER SET utf8 COLLATE utf8_general_ci;"
-	mysql -h 127.0.0.1 -u root --password=root liberatedliving < $TMP_FILE
-else
-	echo "WARNING: local liberatedliving database not loaded - file does not exists"
-fi
+# 	echo "Importing/overriding liberatedliving DB from $DB_IMPORT_FILE"
+# 	mysql -h 127.0.0.1 -u root --password=root -e "CREATE DATABASE liberatedliving CHARACTER SET utf8 COLLATE utf8_general_ci;"
+# 	mysql -h 127.0.0.1 -u root --password=root liberatedliving < $TMP_FILE
+# else
+# 	echo "WARNING: local liberatedliving database not loaded - file does not exists"
+# fi
 
-echo "Setting up LiberatedLiving user"
-mysql -h 127.0.0.1 -u root --password=root mysql < /vagrant/conf/grants.sql
+# echo "Setting up LiberatedLiving user"
+# mysql -h 127.0.0.1 -u root --password=root mysql < /vagrant/conf/grants.sql
 
 echo "Restart apache"
 service apache2 restart
 
-echo "######## COPYING LOCAL CONFIG FILES ##############"
 
-AUTOLOAD=/vagrant/sites/liberated-living/config/autoload
-# composer install
-echo "Copy local config files"
-if [ ! -f $AUTOLOAD/database.local.php ]; then
-	cp /vagrant/conf/database.local.php $AUTOLOAD;
-else
-	echo "WARNING: local database config exists, didn't copy the template"
-fi
-if [ ! -f $AUTOLOAD/mail.config.local.php ]; then
-	cp /vagrant/conf/mail.config.local.php $AUTOLOAD;
-else
-	echo "WARNING: local mail config exists, didn't copy the template"
-fi
-if [ ! -f $AUTOLOAD/stripe.local.php ]; then
-	cp /vagrant/conf/stripe.local.php $AUTOLOAD;
-else
-	echo "WARNING: local stripe config exists, didn't copy the template"
-fi
+## NEED TO CHANGE FOR LARAVEL
+
+# echo "######## COPYING LOCAL CONFIG FILES ##############"
+
+# AUTOLOAD=/vagrant/sites/liberated-living/config/autoload
+# # composer install
+# echo "Copy local config files"
+# if [ ! -f $AUTOLOAD/database.local.php ]; then
+# 	cp /vagrant/conf/database.local.php $AUTOLOAD;
+# else
+# 	echo "WARNING: local database config exists, didn't copy the template"
+# fi
+# if [ ! -f $AUTOLOAD/mail.config.local.php ]; then
+# 	cp /vagrant/conf/mail.config.local.php $AUTOLOAD;
+# else
+# 	echo "WARNING: local mail config exists, didn't copy the template"
+# fi
+# if [ ! -f $AUTOLOAD/stripe.local.php ]; then
+# 	cp /vagrant/conf/stripe.local.php $AUTOLOAD;
+# else
+# 	echo "WARNING: local stripe config exists, didn't copy the template"
+# fi
 
 # BS
 # echo "Install GitHub API access token so we don't get bandwidth restriction"
